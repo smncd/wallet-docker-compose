30fe01d2a39df702053b93724026091bfa081282
did <did@localhost>
2025-12-09 10:50:17 +0100

    fix backend db sql inject, add frontend .env vars, update backend and frontend versions

--- components/wallet-backend/init-sql.sh
+++ components/wallet-backend/init-sql.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 SQL_DATA="""
-DELETE FROM trusted_root_certificate; INSERT INTO trusted_root_certificate (certificate) VALUES ('${WALLET_BACKEND_TRUSTED_ROOT}');
-DELETE FROM credential_issuer; INSERT INTO credential_issuer (credentialIssuerIdentifier, clientId, visible) VALUES ('${WALLET_ISSUER_URL}', '1233', 1);
-DELETE FROM verifier; INSERT INTO verifier (name, url) VALUES ('Friendly Demo Verifier', '${WALLET_VERIFIER_URL}');
+INSERT INTO trusted_root_certificate (certificate) SELECT '${WALLET_BACKEND_TRUSTED_ROOT}' WHERE NOT EXISTS (SELECT 1 FROM trusted_root_certificate WHERE certificate = '${WALLET_BACKEND_TRUSTED_ROOT}');
+INSERT INTO credential_issuer (credentialIssuerIdentifier, clientId, visible) SELECT '${WALLET_ISSUER_URL}', '1233', 1 WHERE NOT EXISTS (SELECT 1 FROM credential_issuer WHERE credentialIssuerIdentifier = '${WALLET_ISSUER_URL}');
+INSERT INTO verifier (name, url) SELECT 'Friendly Demo Verifier', '${WALLET_VERIFIER_URL}' WHERE NOT EXISTS (SELECT 1 FROM verifier WHERE url = '${WALLET_VERIFIER_URL}');
 """
 
 echo "Executing: $SQL_DATA"
